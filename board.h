762b743790049267ae186461b3f5e86ba323f976
udit01 <uditjainiitd@gmail.com>
2018-09-11 10:35:05 +0530

    Added node ranges to board.h

--- board.h
+++ board.h
@@ -5,8 +5,8 @@ class Node{
     public:
         int row, col;
         /*
-        Row varies from 0 - 9 
-        Col varies from a - k ( ie 0 to 9)
+        Row varies from 1- 11 (ie 0 to 10 max) 
+        Col varies from a - k (ie 0 to 10 max)
         */
        int color; 
        /* Color can have 0 1 or 2 corresponding to 
@@ -23,8 +23,24 @@ class Board
 private:
 
 public:
-			// write the names of data members to be used and member functions required along with why/what to do in it
 	Board();
+    /*
+    Nodes have the following ranges
+    Col  ->   Row-range
+    a 0  ->   0 - 3 
+    b 1  ->   0 - 6 
+    c 2  ->   0 - 7 
+    d 3  ->   0 - 8 
+    e 4  ->   0 - 9 
+    f 5  ->   0 - 8  // central col
+    g 6  ->   0 - 9 
+    h 7  ->   0 - 8 
+    i 8  ->   0 - 7 
+    j 9  ->   0 - 6 
+    k 10 ->   0 - 3 
+    */
+   
+    
 	
 };
 
---
+++ utils.cpp
@@ -0,0 +1,9 @@
+#include<utils.h>
+
+static vector<Move> Move::convertToStd(String s){
+    
+}
+
+static string Move::convertToHexagonal( vector<Move> moves){
+
+}
