9bb3151450be61b986d174c5542deda861b4530d
udit01 <uditjainiitd@gmail.com>
2018-09-11 11:06:21 +0530

    Added split logic

--- utils.cpp
+++ utils.cpp
@@ -1,7 +1,22 @@
 #include<utils.h>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
 
-static vector<Move> Move::convertToStd(String s){
-    
+void split(const std::string& str, Container& cont, char delim = ' ')
+{
+    std::stringstream ss(str);
+    std::string token;
+    while (std::getline(ss, token, delim)) {
+        cont.push_back(token);
+    }
+}
+
+static vector<Move> Move::convertToStd(string move){
+    vector<string> splices;
+    split(move, splices);
+
 }
 
 static string Move::convertToHexagonal( vector<Move> moves){
