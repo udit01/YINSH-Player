1be0d1bb8f92e1cd184bbda80420a44b46fa2dd6
mayank19j <mayank19j@gmail.com>
2018-09-12 08:25:47 +0530

    changes in move class

--- move.h
+++ move.h
@@ -9,9 +9,12 @@ class move
 		Node nodes[11][11];		//scenerio for moves
 		char ** moves;			//strings of moves
 		int totalMoves,currmove;
+		int flag;
 	public:
-		char ** poss_moves(int color);
-		//find possible moves of given color
+		move();	//sets pointers to null
+		move(Node **curr_nodes);	//set pointers to null and sets nodes
+		int poss_moves(int color);
+		//find possible moves of given color and return no. of moves found
 }
 
 #endif
