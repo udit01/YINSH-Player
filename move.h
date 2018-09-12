2d18fc78b379ca1ee9478decc5b7670edb3b0aed
mayank19j <mayank19j@gmail.com>
2018-09-12 09:17:24 +0530

    working on move file

--- move.h
+++ move.h
@@ -4,7 +4,7 @@
 
 #include "node.h"	// define it seperately
 
-class move
+class Move
 {
 		Node nodes[11][11];		//scenerio for moves
 		char ** moves;			//strings of moves
@@ -15,8 +15,9 @@ class move
 	public:
 		move();	//sets pointers to null
 		move(Node **curr_nodes);	//set pointers to null and sets nodes
-		node node getNode(int row,int coloumn);
-		void setNode(int row,int coloumn,Node node);	//sets particular configuration at specified row coloumn node
+		~move();	//frees all memory allocated
+		Node node getNode(int row,int coloumn);
+		void setNode(int row,int coloumn,Node node);	//sets particular configuration at specified row coloumn node and moves to null
 		int poss_moves(int color);
 		//find possible moves of given color and return no. of moves found
 		string getNextMove();	//returns next move in array
