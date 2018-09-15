389caa0bb222e7ee04bfd7e439e7c91d0d6d8d67
mayank19j <mayank19j@gmail.com>
2018-09-15 15:35:00 +0530

    doing sh file changes

---
+++ learner.cpp
@@ -0,0 +1,15 @@
+#include<fstream>
+#include<iostream>
+
+using namespace std;
+
+int main( int argc, char **argv)
+{
+    if(argc<4)
+    {
+        cout<<"run in following format/n ./file loser.txt winner.txt resulting.txt"<<endl;
+        exit(0);
+    }
+    //else
+    
+}
--- player.cpp
+++ player.cpp
@@ -47,6 +47,7 @@ int main( int argc, char **argv){
                  weights[i]=stod(w);
              }
          }
+         readfile.close();
     }
     cin >> player;
     cin >> n;
