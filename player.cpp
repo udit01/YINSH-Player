3096bc351b670197a550fc41396c85d320a0436d
udit01 <uditjainiitd@gmail.com>
2018-09-16 17:20:15 +0530

    Better debug statments in player

--- player.cpp
+++ player.cpp
@@ -14,19 +14,15 @@ int main( int argc, char **argv){
     double weights[5];
     //Firstly , server_string is forwarded to process
     //server string has : <player_id> <num_rings> <time> 
-    // string server_string;
-    // cin >> server_string;
-    // getline(cin, server_string);
-    // cout << server_string;
+    
     cin >> player;
     cin >> n;
     cin >> time;
-    // cerr<<"inputs taken well"<<endl;
-    // cout << "P 5 7"<< endl;
+    string temp;
+    getline(cin, temp);
+
     timing *timer=new timing(time);
-    // cerr<<"timer object created"<<endl;
 	Game* game= new Game(n,timer,argc,argv); //creates an object of game class
-    // cerr<<"game object created"<<endl;
     string opp_move,my_move;
     
     if (player == 1){
@@ -40,18 +36,25 @@ int main( int argc, char **argv){
         // cin >> move;
         timer->stop();
         getline(cin, opp_move);
-        cerr<<opp_move<<endl;
 	    timer->resume();
+        cerr << "Inside CPP file : Opponent tried to play : " <<  opp_move  << "  : Calling play MOVE"<< endl ;
 
-        cerr << "Inside CPP file : Opponent played : " <<  opp_move << endl ;
 
 
         game->playmove(convertToStd(opp_move), 3 - player);// a call to a fuction of Game
 
+        cerr << "Inside CPP file : PLAY move finished : Printing board" << endl ;
+
         game->board->printBoard();
+
+        cerr << "Inside CPP file : Getting MY MOVE " << endl ;
+
         my_move = convertToHexagonal(game->getMove(player));
         
+        cerr << "Inside CPP file : I tried to play : " <<  my_move  << "  : COUTING "<< endl ;
         cout << my_move;  
+
+
 
 
     }
